8d1bd4c3be682a91c83fc945b5e74e660e85e319
Patrick Beaume <p.beaume@gmail.com>
2024-04-14 14:05:40 +0200

    don't allow to reserve an archive if the player already have one

--- rules/src/rules/ReserveRule.ts
+++ rules/src/rules/ReserveRule.ts
@@ -1,4 +1,5 @@
 import { isMoveItemType, ItemMove, PlayerTurnRule } from '@gamepark/rules-api'
+import { isArchive } from '../material/Card'
 import { LocationType } from '../material/LocationType'
 import { MaterialType } from '../material/MaterialType'
 import { BoardHelper } from './helper/BoardHelper'
@@ -22,9 +23,19 @@ export class ReserveRule extends PlayerTurnRule {
   }
 
   get boardCards() {
+    const hasArchive = this.hasArchive
     return this
       .material(MaterialType.Card)
       .location(LocationType.BoardCard)
+      .filter((item) => !hasArchive || !isArchive(item.id.front))
+  }
+
+  get hasArchive() {
+    return this
+      .material(MaterialType.Card)
+      .player(this.player)
+      .id(({ front }: any) => front && isArchive(front))
+      .length === 1
   }
 
 }
