5a7182268a6c56f127b36320d0bb9303ba7247d0
Patrick Beaume <p.beaume@gmail.com>
2024-04-14 12:12:59 +0200

    modify victory point icon

--- app/src/material/BorealCardDescription.tsx
+++ app/src/material/BorealCardDescription.tsx
@@ -1,46 +1,6 @@
 /** @jsxImportSource @emotion/react */
-import { css, Interpolation, Theme } from '@emotion/react'
 import { Card, CardBack } from '@gamepark/boreal/material/Card'
-import { LocationType } from '@gamepark/boreal/material/LocationType'
-import { MaterialType } from '@gamepark/boreal/material/MaterialType'
-import { ScoreHelper } from '@gamepark/boreal/rules/helper/ScoreHelper'
-import { CardDescription, ItemContext } from '@gamepark/react-game'
-import YellowStart1 from '../images/cards/yellow_start_1.jpg'
-import YellowStart2 from '../images/cards/yellow_start_2.jpg'
-import YellowStart3 from '../images/cards/yellow_start_3.jpg'
-import Yellow4 from '../images/cards/yellow_4.jpg'
-import Yellow5 from '../images/cards/yellow_5.jpg'
-import Yellow6 from '../images/cards/yellow_6.jpg'
-import Yellow7 from '../images/cards/yellow_7.jpg'
-import Yellow8 from '../images/cards/yellow_8.jpg'
-import Yellow9 from '../images/cards/yellow_9.jpg'
-import RedStart1 from '../images/cards/red_start_1.jpg'
-import RedStart2 from '../images/cards/red_start_2.jpg'
-import RedStart3 from '../images/cards/red_start_3.jpg'
-import Red4 from '../images/cards/red_4.jpg'
-import Red5 from '../images/cards/red_5.jpg'
-import Red6 from '../images/cards/red_6.jpg'
-import Red7 from '../images/cards/red_7.jpg'
-import Red8 from '../images/cards/red_8.jpg'
-import Red9 from '../images/cards/red_9.jpg'
-import BlueStart1 from '../images/cards/blue_start_1.jpg'
-import BlueStart2 from '../images/cards/blue_start_2.jpg'
-import BlueStart3 from '../images/cards/blue_start_3.jpg'
-import Blue4 from '../images/cards/blue_4.jpg'
-import Blue5 from '../images/cards/blue_5.jpg'
-import Blue6 from '../images/cards/blue_6.jpg'
-import Blue7 from '../images/cards/blue_7.jpg'
-import Blue8 from '../images/cards/blue_8.jpg'
-import Blue9 from '../images/cards/blue_9.jpg'
-import GreenStart1 from '../images/cards/green_start_1.jpg'
-import GreenStart2 from '../images/cards/green_start_2.jpg'
-import GreenStart3 from '../images/cards/green_start_3.jpg'
-import Green4 from '../images/cards/green_4.jpg'
-import Green5 from '../images/cards/green_5.jpg'
-import Green6 from '../images/cards/green_6.jpg'
-import Green7 from '../images/cards/green_7.jpg'
-import Green8 from '../images/cards/green_8.jpg'
-import Green9 from '../images/cards/green_9.jpg'
+import { CardDescription } from '@gamepark/react-game'
 import Archive1 from '../images/cards/archive_1.jpg'
 import Archive2 from '../images/cards/archive_2.jpg'
 import Archive3 from '../images/cards/archive_3.jpg'
@@ -49,9 +9,44 @@ import Archive5 from '../images/cards/archive_5.jpg'
 import Archive6 from '../images/cards/archive_6.jpg'
 import Archive7 from '../images/cards/archive_7.jpg'
 import Archive8 from '../images/cards/archive_8.jpg'
-import StartBack from '../images/cards/start_back.jpg'
+import Blue4 from '../images/cards/blue_4.jpg'
+import Blue5 from '../images/cards/blue_5.jpg'
+import Blue6 from '../images/cards/blue_6.jpg'
+import Blue7 from '../images/cards/blue_7.jpg'
+import Blue8 from '../images/cards/blue_8.jpg'
+import Blue9 from '../images/cards/blue_9.jpg'
+import BlueStart1 from '../images/cards/blue_start_1.jpg'
+import BlueStart2 from '../images/cards/blue_start_2.jpg'
+import BlueStart3 from '../images/cards/blue_start_3.jpg'
+import Green4 from '../images/cards/green_4.jpg'
+import Green5 from '../images/cards/green_5.jpg'
+import Green6 from '../images/cards/green_6.jpg'
+import Green7 from '../images/cards/green_7.jpg'
+import Green8 from '../images/cards/green_8.jpg'
+import Green9 from '../images/cards/green_9.jpg'
+import GreenStart1 from '../images/cards/green_start_1.jpg'
+import GreenStart2 from '../images/cards/green_start_2.jpg'
+import GreenStart3 from '../images/cards/green_start_3.jpg'
+import Red4 from '../images/cards/red_4.jpg'
+import Red5 from '../images/cards/red_5.jpg'
+import Red6 from '../images/cards/red_6.jpg'
+import Red7 from '../images/cards/red_7.jpg'
+import Red8 from '../images/cards/red_8.jpg'
+import Red9 from '../images/cards/red_9.jpg'
+import RedStart1 from '../images/cards/red_start_1.jpg'
+import RedStart2 from '../images/cards/red_start_2.jpg'
+import RedStart3 from '../images/cards/red_start_3.jpg'
 import StandardBack from '../images/cards/standard_back.jpg'
-import { MaterialItem } from '@gamepark/rules-api'
+import StartBack from '../images/cards/start_back.jpg'
+import Yellow4 from '../images/cards/yellow_4.jpg'
+import Yellow5 from '../images/cards/yellow_5.jpg'
+import Yellow6 from '../images/cards/yellow_6.jpg'
+import Yellow7 from '../images/cards/yellow_7.jpg'
+import Yellow8 from '../images/cards/yellow_8.jpg'
+import Yellow9 from '../images/cards/yellow_9.jpg'
+import YellowStart1 from '../images/cards/yellow_start_1.jpg'
+import YellowStart2 from '../images/cards/yellow_start_2.jpg'
+import YellowStart3 from '../images/cards/yellow_start_3.jpg'
 
 export class BorealCardDescription extends CardDescription {
   height = 7
@@ -110,24 +105,24 @@ export class BorealCardDescription extends CardDescription {
     [Card.Archive7]: Archive7,
     [Card.Archive8]: Archive8,
   }
-  getItemExtraCss(item: MaterialItem, _context: ItemContext): Interpolation<Theme> {
-    const { rules, index } = _context
-    if (item.location.type !== LocationType.Pyramid) return
-    const cardScore = new ScoreHelper(rules.game, item.location.player!).getCardScore(rules.material(MaterialType.Card).index(index))
-    return css`
-      &:after {
-        content: '${cardScore}';
-        height: 100%;
-        width: 100%;
-        position: absolute;
-        top: 0;
-        font-size: 3em;
-        left: 0;
-        color: black;
-        transform: translateZ(10px);
-      }
-    `
-  }
+  // getItemExtraCss(item: MaterialItem, _context: ItemContext): Interpolation<Theme> {
+  //   const { rules, index } = _context
+  //   if (item.location.type !== LocationType.Pyramid) return
+  //   const cardScore = new ScoreHelper(rules.game, item.location.player!).getCardScore(rules.material(MaterialType.Card).index(index))
+  //   return css`
+  //     &:after {
+  //       content: '${cardScore}';
+  //       height: 100%;
+  //       width: 100%;
+  //       position: absolute;
+  //       top: 0;
+  //       font-size: 3em;
+  //       left: 0;
+  //       color: black;
+  //       transform: translateZ(10px);
+  //     }
+  //   `
+  // }
 }
 
 export const borealCardDescription = new BorealCardDescription()
