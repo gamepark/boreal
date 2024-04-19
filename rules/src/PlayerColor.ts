4b911a2c7242bf5fb220f8c398da3f15b83c16e6
Romain Fromi <romain.fromi@gmail.com>
2024-04-19 10:32:02 +0200

    Swap player colors

--- rules/src/PlayerColor.ts
+++ rules/src/PlayerColor.ts
@@ -1,7 +1,7 @@
 import { isEnumValue } from '@gamepark/rules-api'
 
 export enum PlayerColor {
-  Black = 1, White
+  White = 1, Black
 }
 
 export const playerColors = Object.values(PlayerColor).filter(isEnumValue)
